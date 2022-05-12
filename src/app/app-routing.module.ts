79e63315fe30d41668e2f165684e4c1ffdf605fd
armando <arman.2.r@gmail.com>
2022-05-12 00:47:40 -0500

    cambia en la ruta por defecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'catalogo', loadChildren: () => import('./catalogo/catalogo.module').then(m => m.CatalogoModule) }];
+const routes: Routes = [{ path: '', redirectTo: '/catalogo', pathMatch: 'full' },{ path: 'catalogo', loadChildren: () => import('./catalogo/catalogo.module').then(m => m.CatalogoModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
